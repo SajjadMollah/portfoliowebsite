a619671d8d60c7729b07cb25e2708b15e50fbf81
SajjadMollah <sajjadmollah314@gmail.com>
2024-03-05 04:35:13 +0000

    Completed Contact page

--- my_website/my_website/static/script.js
+++ my_website/my_website/static/script.js
@@ -1,6 +1,23 @@
 console.log('Hello world')
 
+
+function showHome() {
+  var aboutPage= document.getElementsByClassName("main-page")[0];
+  aboutPage.scrollIntoView({ block: 'start', behavior: 'smooth' });
+}
+
 function showAbout() {
-    var aboutPage= document.getElementById(".about-page");
-    aboutPage.scrollIntoView();
+    var aboutPage= document.getElementsByClassName("about-page")[0];
+    aboutPage.scrollIntoView({ block: 'start', behavior: 'smooth' });
   }
+
+  function showProjects() {
+    var aboutPage= document.getElementsByClassName("projects-page")[0];
+    aboutPage.scrollIntoView({ block: 'start', behavior: 'smooth' });
+  }
+
+  function showContacts() {
+    var aboutPage= document.getElementsByClassName("contacts-page")[0];
+    aboutPage.scrollIntoView({ block: 'start', behavior: 'smooth' });
+  }
+  
